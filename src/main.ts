6a33380584e1b4dbe6ff464dfdde6a2077ddf65a
rafael <rafael.santos7520@gmail.com>
2023-10-09 10:09:35 -0300

    alter cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Api Imoveis')
