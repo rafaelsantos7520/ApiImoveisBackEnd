d2ade1bcf5a5ad7406f8903e74a76bdf1351b26e
unknown <rafael>
2023-12-14 11:00:13 -0300

    update main

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,9 @@ import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { PropertiesModule } from './modules/properties/properties.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { CorsModule } from '@nestjs/common';
 
 @Module({
-  imports: [
-    UsersModule,
-    PropertiesModule,
-    AuthModule,
-    CorsModule.forRoot({
-      origin: 'http://localhost:3000', // Substitua com o domínio do seu frontend em produção
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-      credentials: true, // Permite enviar cookies
-    }),
-  ],
+  imports: [UsersModule, PropertiesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
+
+  // Habilitar o CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // Substitua com o domínio do seu frontend em produção
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+    credentials: true, // Permite enviar cookies
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Api Imoveis')
-    .setDescription('api de corretora de imoveis')
+    .setDescription('API de corretora de imoveis')
     .setVersion('1.0')
     .addBearerAuth()
     .addBasicAuth()
